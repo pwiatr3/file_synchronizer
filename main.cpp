5cdf1985e8195eb18f4c2962f5906d45eecd8acd
pwiatr3 <pwiatr3@yahoo.pl>
2023-01-03 16:39:44 +0100

    clear main.cpp

--- main.cpp
+++ main.cpp
@@ -3,30 +3,22 @@
 #include <string>
 #include <filesystem>
 #include "fileInfo.hpp"
+#include "fileContainer.hpp"
 
 namespace fs = std::filesystem;
 
-void filesVector(const fs::path& dir_path, std::vector<fs::path>& files);
-void printAllFiles(const std::vector<fs::path>& files);
+void scan(const fs::path& dir_path, std::vector<FileInfo>& allFileInfo);
 
 int main() {
+    // fs::path syncDirectoriesPath {fs::current_path() / "DirForSync"};
     fs::path syncDirectoriesPath {fs::current_path().parent_path() / "DirForSync"};
-    std::cout << syncDirectoriesPath << '\n';
-    std::vector<fs::path> files;
-    std::vector<fileInfo> fileInfo
-    filesVector(syncDirectoriesPath, files);
-    printAllFiles(files);
+    FileContainer fileContainer;
+    
+    std::cout << "Sync directory path: " << syncDirectoriesPath << '\n';
+    // scan(syncDirectoriesPath, allFilesInfo);
 
     return 0;
 }
 
-void filesVector(const fs::path& dir_path, std::vector<fs::path>& files) {
-    std::copy_if(fs::recursive_directory_iterator(dir_path), {}, std::back_insert_iterator(files), [](const auto& entry){ 
-        return entry.is_regular_file(); });
-}
 
-void printAllFiles(const std::vector<fs::path>& files) {
-   for(const auto& filePath : files) {
-        std::cout << filePath << '\n';
-   } 
-}
+
